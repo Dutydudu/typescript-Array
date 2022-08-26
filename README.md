35b493d9ab6bcf92d480fdb88b887b05d8b93372
Dutydudu <dut.dudu123@gmail.com>
2022-08-26 19:42:45 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-jmeeqc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-jmeeqc)
---
+++ index.ts
@@ -0,0 +1,10 @@
+let list: number[] = [1,2,3];
+
+let otherList: Array<number> = [1,2,3];
+
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `
+<h1>Array declaration</h1>
+<h2>List: ${list}</h2>
+<h2>other list: ${otherList}</h2>
+`;
