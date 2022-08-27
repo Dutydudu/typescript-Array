1216ea9ac56a2ad82a5169127ac6fa0eb5d8103d
Dutydudu <dut.dudu123@gmail.com>
2022-08-26 21:15:27 -0300

    class, events, assertions

--- index.ts
+++ index.ts
@@ -67,3 +67,93 @@ appDiv5.innerHTML = `
 <h2>unusable: ${unusable}</h2>
 <h2>myFunction: ${myFunction}</h2>
 `;
+/*
+declare function create(o: object | null): void;
+create({ prop: 0 }); //ok
+create(null); //ok
+create(undefined); //ok
+*/
+
+/*
+create(42); erro
+create("string"); erro
+create(false); erro
+
+const appDiv6: HTMLElement = document.getElementById('object');
+appDiv6.innerHTML = `
+<h1>Object</h1>
+`;
+*/
+
+let someValue: any = 'this is a string';
+let strLength: number = (<string>someValue).length;
+
+let someValue2: any = 'this is a string';
+let strLength2: number = (someValue2 as string).length;
+
+const appDiv7: HTMLElement = document.getElementById('assertions');
+appDiv7.innerHTML = `
+<h1>Type assertions</h1>
+`;
+
+/*
+function sum(n1: number, n2: number): number {
+  return n1 + n2;
+}
+function fullName(pessoa: { name: string; lastname: string }): string {
+  return pessoa.name + ' ' + pessoa.lastname;
+}
+
+const appDiv8: HTMLElement = document.getElementById('function');
+appDiv8.innerHTML = `
+<h1>Functions</h1>
+<h2>Soma de 2 e 3: ${sum(2, 3)}</h2>
+<h2>Soma de 11 e 329: ${sum(11, 329)}</h2>
+<h2>Meu nome completo: ${fullName({
+  lastname: 'Duarte',
+  name: 'Luis Ed.',
+})}</h2>
+`;
+*/
+
+//import stylesheets
+import './style.css';
+
+let title: string = 'Events';
+let paragraph: string = 'Blablabla...';
+
+let counter = 0;
+
+let intervalId = setInterval(() => {
+  counter = counter + 1;
+  const appP: HTMLElement = document.getElementById('app.p');
+  appP.innerHTML = 'Contador: ' + counter;
+}, 1000);
+
+const btn = document.getElementById('app.btn');
+btn?.addEventListener('click', alertMethod);
+function alertMethod(this: HTMLElement, ev: Event) {
+  alert('Alertando com typescript');
+}
+
+// Write TypeScript code!
+const appH1: HTMLElement = document.getElementById('app.h1');
+appH1.innerHTML = title;
+
+class Greeter {
+  greeting: string;
+  constructor(message: string) {
+    this.greeting = message;
+  }
+  greet() {
+    return 'Hello, ' + this.greeting;
+  }
+}
+
+let greeter = new Greeter('Class');
+
+const appDiv9: HTMLElement = document.getElementById('class');
+appDiv9.innerHTML = `
+<h1>Class</h1>
+<h2>${greeter.greet()}!</h2>
+`;
